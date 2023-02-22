afc287122221f1f6e54e9f3b485854d051aabf02
AmineZouaghi <Zouaghimedamine2001@gmail.com>
2023-02-21 18:57:51 -0800

    added some script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * _abs: calculate abs value
+ *
+ * @int: a given int
+ *
+ * Return: the abs value
+ */
+
+int _abs(int)
+{
+	if (int > 0) 
+		return int;
+	else
+		return (-int);
+}
