a4ef85b09eb1b726edf1c1b90e76bb5322d6c25f
AmineZouaghi <Zouaghimedamine2001@gmail.com>
2023-02-23 15:27:54 -0800

    added some code

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal shape using '\' and ending with a new line
+ * @n: number of '\'
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+		for (j = 0; j < n; j++)
+		{
+			for (i = 0 ; i < j; i++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
