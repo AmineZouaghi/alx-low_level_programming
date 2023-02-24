ea90709ad8efacb5d124a3dc22ad0dbf5173675d
AmineZouaghi <Zouaghimedamine2001@gmail.com>
2023-02-23 17:08:32 -0800

    added some code

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ * @size: input (size of the triangle)
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i, j, k;
+		i = 1;
+		while (i <= size)
+		{
+			for (k = size - i; k >= 1; k--)
+				_putchar(' ');
+			for (j = 0; j < i; j++)
+				_putchar('#');
+			i++;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
