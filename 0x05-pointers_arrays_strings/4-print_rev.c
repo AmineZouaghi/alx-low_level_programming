d3df3312912a1fb5170a50bb245c7000f9177739
AmineZouaghi <Zouaghimedamine2001@gmail.com>
2023-02-27 14:39:18 -0800

    added some script

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,13 @@
-#include <stdio.h>                                                 
-#include <string.h>                                                
-#include "main.h"                                                  
-                                                                   
-/**                                                                
-*print_rev - Prints a string in reverse order                      
-*@s: String to reverse                                             
-*Return: Nothing                                                   
-*/                                                                 
-                                                                   
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+*print_rev - Prints a string in reverse order
+*@s: String to reverse
+*Return: Nothing
+*/
+
 void print_rev(char *s)
 {
 	int len = strlen(s) ;
