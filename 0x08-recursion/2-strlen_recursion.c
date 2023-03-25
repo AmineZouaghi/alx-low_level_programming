228faf4c485c33f356e1c8b2b5c729993a1ce2c4
AmineZouaghi <Zouaghimedamine2001@gmail.com>
2023-03-25 16:08:37 -0700

    added some code

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,19 +2,18 @@
 
 /**
  * _strlen_recursion - Returns the lenght of a string
- * 
+ *
  * @s: given pointer
  *
  * Return: int
  */
-
 int _strlen_recursion(char *s)
 {
 	int i = 0;
 
 	if (*s != '\0')
 	{
-		i = i+_strlen_recursion(s+1)+1;
+		i = i + _strlen_recursion(s + 1) + 1;
 	}
-		return( i );
+		return (i);
 }
