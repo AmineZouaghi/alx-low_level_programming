04c770ef2823f8f56facbef796a5d909cf325238
AmineZouaghi <Zouaghimedamine2001@gmail.com>
2023-03-25 17:33:25 -0700

    added some code

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Returns the sqrt of a given integer
+ *
+ * @n: the given integer
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{	
+	return _sqrt(n,1);
+}
+
+/**
+ * sqrt - Returns the sqrt of a given integer
+ *
+ * @n: the given integer
+ * @val:the value to test with
+ *
+ * Return: int
+ */
+
+int _sqrt(int n, int val)
+{
+	if (val * val == n)
+		return val;
+	else if (val * val > n)
+		return (-1);
+	else
+		return _sqrt(n, val + 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-void _puts_recursion(char *s);
+void _puts_recursion(char *);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -8,3 +8,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int wildcmp(char *s1, char *s2);
 int is_palindrome(char *s);
+int _sqrt(int n, int val);
